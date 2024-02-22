7c91a50b9ff51123ed7599040baeedbbfbf590cd
sudhir1507 <skadam1507@gmail.com>
2024-02-22 23:31:59 +0530

    all added

---
+++ FirstJulyJava/src/arrayuse/AnnonymusArray.java
@@ -0,0 +1,22 @@
+package arrayuse;
+
+public class AnnonymusArray {
+	public static void showArr(int a[]) {
+		for(int i:a) {
+			System.out.print(i+" ");
+		}
+	}
+	public static int[] returnArr() {
+		return new int[] {1,2,3,4,5,7};
+	}
+	
+	public static void main(String[] args) {
+		showArr(new int[] {1,2,3,4,6});
+	    int a[]=returnArr();
+	    System.out.println();
+	    for(int i:a) {
+	    	System.out.print(i+" ");
+	    }
+	}
+
+}
